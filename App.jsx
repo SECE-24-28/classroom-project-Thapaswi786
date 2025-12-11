ae84e96bc220fa97bfcc14c2debe13d249e7431a
Thapaswi786 <thapaswi.sj2024cse@sece.ac.in>
2025-12-11 08:31:23 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,37 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Header';
-import { Body } from './Body';
-import { Footer } from './Footer';
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Gallary from './Gallary'
+import PostPage from './PostPage'
+import Post from './Post'
+import NewPost from './NewPost'
 
 function App() 
 {
-      const [list,setList] =useState([
-        {id:1,sname:"Vinoth",fee:true},
-        {id:2,sname:"Shiva",fee:false},
-        {id:3,sname:"Harish",fee:true}
-      ]);
 
-      const handleDelete=(id)=>
-      {
-      const newList=list.filter((ls)=>ls.id!=id)
-      setList(newList)
-      }
-      const handleCheck=(id)=>
-      {
-         const newList=list.map((ls)=>(ls.id===id)?({...ls,fee:!ls.fee}):(ls))
-         setList(newList)
-      }
-      
   return (
     <>
-        <Header title={"Student List"}/>
-        <Body list={list}
-              handleCheck={handleCheck}
-              handleDelete={handleDelete}
-        />
-           <Footer len={list.length} />
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/gallary">Gallary</Link></li>
+      <li><Link to="/postpage">PostPage</Link></li>
+    </ul>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/gallary" element={<Gallary />} />
+      <Route path="/postpage" element={<PostPage />} >
+              <Route path=":id" element={<Post />} />
+              <Route path="/postpage/newpost" element={<NewPost />} />
+      </Route>
+      
+    </Routes>
     </>
   )
 }
